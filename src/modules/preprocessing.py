960a6cbddd9972b14b5f801de59a75208e0c9698
MelisCakan <cakanmelis27@gmail.com>
2025-04-02 22:09:21 +0300

    function data_normalization name changed to normalize_and scale and function moved upper

--- src/modules/preprocessing.py
+++ src/modules/preprocessing.py
@@ -1,12 +1,12 @@
 import scanpy as sc
 
-def preprocess(adata, target_sum = 1e4, max_value = 10):
-    data_normalization(adata, target_sum, max_value)
-    #other preprocessing methods here
-    return adata
-
-def data_normalization(adata, target_sum, max_value):
+def normalize_and_scale(adata, target_sum, max_value):
     sc.pp.normalize_total(adata, target_sum)
     sc.pp.log1p(adata)
     sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
     sc.pp.scale(adata, max_value)
+
+def preprocess(adata, target_sum = 1e4, max_value = 10):
+    normalize_and_scale(adata, target_sum, max_value)
+    #other preprocessing methods here
+    return adata
