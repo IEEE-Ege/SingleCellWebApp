a9ae7869927e2230f9e97f2990892d72ad2bb246
MelisCakan <cakanmelis27@gmail.com>
2025-04-03 22:48:13 +0300

    try except block has been deleted

--- src/modules/preprocessing.py
+++ src/modules/preprocessing.py
@@ -6,14 +6,13 @@ def normalize_and_scale(adata, target_sum, max_value):
     sc.pp.scale(adata, max_value)
 
     vars_to_check = ["total_counts", "pct_counts_mt"]
-    try:
-        for var in vars_to_check: #checking for the var names if adata has them
-            if var not in adata.obs.columns:
-                raise ValueError(f"{var} is not found in adata.var_names")
-            
-        sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt']) #if adata has the parameters do regress out
-    except Exception as e:
-        print(f"An error has occurred: {e}") 
+
+    for var in vars_to_check: #checking for the var names if adata has them
+        if var not in adata.obs.columns:
+            raise ValueError(f"{var} is not found in adata.var_names")
+        
+    sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt']) #if adata has the parameters do regress out
+
 
 
 def preprocess(adata, target_sum = 1e4, max_value = 10):
