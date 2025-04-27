0c24182f460cc3d9a932dbb8cf7fcb0917c50eb1
Ekin-hub-code <ekinmertsoysal@gmail.com>
2025-04-27 16:06:23 +0300

    Update rank_genes_group.py

--- src/modules/rank_genes_group.py
+++ src/modules/rank_genes_group.py
@@ -1,21 +1,38 @@
+import scanpy as sc
 
-import scanpy as sc 
+# Örnek veri setini yükle
+adata = sc.datasets.pbmc3k()
 
-sc.pp.neighbors(adata)
+# Temel ön işleme
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+adata = adata[:, adata.var.highly_variable]
+sc.pp.scale(adata, max_value=10)
+
+# PCA ve komşuluk hesaplama
+sc.pp.pca(adata, svd_solver='arpack')  # Bu satır uyarıyı çözer
+sc.pp.neighbors(adata, n_pcs=40)
+
+# Clustering (Leiden)
 sc.tl.leiden(adata, resolution=1.0)
-sc.settings.verbosity = 2  # reduce the verbosity
 
+# Verbosity ayarı
+sc.settings.verbosity = 2
+
+# Sonuçları kaydetmek için bir dosya ismi belirle
+results_file = "pbmc3k_rank_genes.h5ad"
+
+# Gen sıralama ve görselleştirme fonksiyonu
 def rank_genes_groups(adata, method='t-test', n_genes=25, sharey=False):
     """
-    -Perform differential expression analysis and plot top marker genes.
-
-    Parameters:
-    - adata: AnnData object
-    - method: Method for differential testing ('t-test', 'wilcoxon', 'logreg', etc.)
-    - n_genes: Number of top genes to plot
-    - sharey: Whether to share the y-axis across plots
+    - Perform differential expression analysis and plot top marker genes.
     """
-
     sc.tl.rank_genes_groups(adata, groupby='leiden', method=method)
     sc.pl.rank_genes_groups(adata, n_genes=n_genes, sharey=sharey)
     adata.write(results_file)
+
+# Fonksiyonu çağır
+rank_genes_groups(adata)
+
+
