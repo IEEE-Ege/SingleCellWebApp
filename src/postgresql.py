b4685f20f3d1cf3e3b3a9ab8d53cfd25f673ddf4
MelisCakan <cakanmelis27@gmail.com>
2025-08-03 21:14:05 +0300

    major fixes to fix an error

--- src/postgresql.py
+++ src/postgresql.py
@@ -45,10 +45,8 @@ def create_jwt_token(username: str) -> str:
 # Define the User Interface (UI) for the Shiny app
 app_ui = ui.page_fluid(
     # Custom CSS styling for the app's appearance
-    ui.tags.head(
-        ui.tags.link(rel="stylesheet", href="frontend/styles.css"),
-        ui.tags.script(src="frontend/app.js"),
-    ),
+    ui.head_content(ui.include_css("frontend/styles.css"),
+                    ui.include_js("frontend/app.js")),
 
 
     # Main application div container
