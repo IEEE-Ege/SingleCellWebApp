d48db65d5f727b9719670fe1e8fca9489d66f8c3
MelisCakan <cakanmelis27@gmail.com>
2025-08-09 13:33:16 +0300

    fixing bugs

--- src/utilities.py
+++ src/utilities.py
@@ -7,11 +7,7 @@ from dotenv import load_dotenv
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Function to create a JWT token
-<<<<<<< HEAD
-def create_jwt_token(username: str) -> str:
-=======
 def create_jwt_token(username: str, SECRET_KEY: str) -> str:
->>>>>>> c681feae35dc8c4c4debbe3d19f152db5c49ea29
     # Payload for the JWT token
     payload = {
         "user": username,
