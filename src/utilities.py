99878b13cf127773be076cf3a9e149ef5f7414eb
MelisCakan <cakanmelis27@gmail.com>
2025-08-09 13:31:34 +0300

    Secret key has been called from .env in utilities to create jwt token

--- src/utilities.py
+++ src/utilities.py
@@ -1,10 +1,13 @@
 # utilities.py
 import jwt
 from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
 
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Function to create a JWT token
-def create_jwt_token(username: str, SECRET_KEY) -> str:
+def create_jwt_token(username: str) -> str:
     # Payload for the JWT token
     payload = {
         "user": username,
