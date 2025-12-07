48a662a79c852ef90b86174b4b0b044699a87a4d
happymealinthebuilding <aazrtncy@gmail.com>
2025-12-07 15:25:48 +0300

    workflow updates

---
+++ melisilkwf.py
@@ -0,0 +1,87 @@
+#üzerine ekleme yapılmamış melisin ilk wf kodu
+
+from prefect import flow, task
+import random
+
+@task
+def upload_dataset():
+    print("Uploading dataset...")
+
+@task
+def store_dataset():
+    print("Storing dataset...")
+
+@task(retries=3, retry_delay_seconds=5)
+def quality_control() -> bool:
+    print("Performing quality control...")
+    qc_passed = random.random() > 0.5
+    if not qc_passed:
+        print("QC failed!")
+        raise ValueError("QC failed!")
+    print("QC succeeded!")
+    return True
+
+@task
+def normalization():
+    print("Normalizing data...")
+
+@task
+def log_gc_errors():
+    print("Logging GC errors...")
+
+@task(retries=3, retry_delay_seconds=5)
+def dimensionality_reduction_pca() -> bool:
+    print("Performing PCA for dimensionality reduction...")
+    if random.random() < 0.5:
+        print("PCA failed!")
+        raise ValueError("PCA failed!")
+    print("PCA succeeded!")
+    return True
+
+@task()
+def clustering():  # adjust parameters eklenecek
+    print("Clustering data...")
+
+@task
+def save_results():
+    print("Saving results...")
+
+@task
+def generate_umap_visualization():
+    print("Generating UMAP visualization...")
+
+@task
+def render_interactive_umap():
+    print("Rendering interactive UMAP...")
+
+@task
+def generate_output_report():
+    print("Generating output report...")
+
+@flow
+def my_flow():
+    upload_dataset()
+    store_dataset()
+    
+    try:
+        qc_success = quality_control().result()
+    except Exception:
+        log_gc_errors()
+        print("QC tekrar denendi ama başarısız oldu. Normalization ve downstream task'ler devam edecek.")
+    
+    normalization()
+    
+    try:
+        pca_success = dimensionality_reduction_pca().result()
+        if pca_success:
+            clustering()
+    except Exception:
+        print("PCA tekrar denendi ama başarısız oldu, clustering atlandı.")
+    
+    save_results()
+    generate_umap_visualization()
+    render_interactive_umap()
+    generate_output_report()
+
+if __name__ == "__main__":
+    my_flow()
